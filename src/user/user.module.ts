465a92a72315b44c5fcaae64b4058ce72ffa8d69
Welly Setiawan Limantoro <welly.eic06g@nctu.edu.tw>
2020-07-15 22:51:40 +0800

    adding authentication module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,10 +3,12 @@ import { UserService } from './service/user.service';
 import { UserController } from './controller/user.controller';
 import { UserEntity } from './models/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([UserEntity])
+    TypeOrmModule.forFeature([UserEntity]),
+    AuthModule
   ],
   providers: [UserService],
   controllers: [UserController]
