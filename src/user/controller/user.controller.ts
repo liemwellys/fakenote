2e37d67f13c5a19cf14f88921fd7767b0dd9674f
Welly Setiawan Limantoro <welly.eic06g@nctu.edu.tw>
2020-07-15 22:48:15 +0800

    adding authentication module

--- src/user/controller/user.controller.ts
+++ src/user/controller/user.controller.ts
@@ -1,16 +1,31 @@
 import { Controller, Post, Body, Get, Param, Delete, Put } from '@nestjs/common';
 import { UserService } from '../service/user.service';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { User } from '../models/user.interface';
+import { map, catchError } from 'rxjs/operators';
+import { access } from 'fs';
 
 @Controller('user')
 export class UserController {
     constructor(private userService: UserService){}
 
     @Post()
-    create(@Body()user: User): Observable<User>{
-        return this.userService.create(user);
+    create(@Body()user: User): Observable<User | Object>{
+        return this.userService.create(user).pipe(
+            map((user: User) => user),
+            catchError(err => of({error: err.message}))
+        );
     }
+
+    @Post('login')
+    login(@Body() user: User): Observable<Object> {
+        return this.userService.login(user).pipe(
+            map((jwt: string) => {
+                return {access_token: jwt};
+            })
+        )
+    }
+
     
     @Get(':id')
     findOne(@Param()params): Observable<User>{
